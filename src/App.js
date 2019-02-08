7cc181c4f2e746ea61a78a9e8f223bcd7869d20e
James de Winton <james.dewinton@gmail.com>
2019-02-08 14:33:00 +0000

    react routing dependacies now working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Route } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
 
 
-const App = () => <div> Hello World </div>;
-
+const App = () => (
+  <div>
+    <Route path="/" exact component={HomePage} />
+    </div>
+);
 export default App;
---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const HomePage = (props) => (
+  <div>
+    <h1> HomePage </h1>
+    <Link to="/login">Login</Link>
+    </div>
+);
+export default HomePage;
