fd952196d27b778fbbbd0c82f20e0ee7064a6287
James de Winton <james.dewinton@gmail.com>
2019-02-10 14:07:22 +0000

    login page created

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -10,13 +10,34 @@ class LoginForm extends React.Component {
     loading: false,
     errors: {}
   };
+  onChange = e =>
+    this.setState({
+      data: { ...this.state.data, [e.target.name]: e.target.value }
+    });
 
   render() {
+    const { data } = this.state;
     return (
       <Form>
       <Form.Field>
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" placeholder="example@example.com"/>
+        <input type="email"
+          id="email"
+          name="email"
+          placeholder="example@example.com"
+          value={data.email}
+          onChange={this.onChange}
+          />
+      </Form.Field>
+      <Form.Field>
+        <label htmlFor="password">Password</label>
+        <input type="password"
+          id="password"
+          name="password"
+          placeholder="Make it secure"
+          value={data.password}
+          onChange={this.onChange}
+          />
       </Form.Field>
         <Button primary>Login</Button>
       </Form>
