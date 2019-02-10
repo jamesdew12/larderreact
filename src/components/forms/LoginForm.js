f8bdd222221aba1b3f86e0ec20ecf711374651c9
James de Winton <james.dewinton@gmail.com>
2019-02-10 15:44:53 +0000

    login page finalized

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Form, Button } from 'semantic-ui-react';
 import Validator from 'validator';
 import InlineError from '../messages/InlineError';
@@ -17,7 +18,10 @@ class LoginForm extends React.Component {
     });
   onSubmit = () => {
     const errors = this.validate(this.state.data);
-    this.setState({ errors })
+    this.setState({ errors });
+    if (Object.keys(errors).length === 0){
+      this.props.submit(this.state.data);
+    }
   };
 
   validate = (data) => {
@@ -58,5 +62,8 @@ class LoginForm extends React.Component {
     );
   }
 }
+LoginForm.propTypes = {
+  submit: PropTypes.func.isRequired
+};
 
 export default LoginForm;
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import LoginForm from "../forms/LoginForm";
 
-const LoginPage = () => (
-  <div>
-    <h1> Login Page </h1>
+class LoginPage extends React.Component {
+  submit = data => {
+    console.log(data);
+  };
+  render() {
+    return (
+      <div>
+        <h1> Login Page </h1>
 
-    <LoginForm />
-  </div>
-);
+        <LoginForm submit={this.submit}/>
+      </div>
+
+    );
+  }
+}
+
 export default LoginPage;
