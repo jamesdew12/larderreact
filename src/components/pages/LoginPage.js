30d438c7c0050a9cc6bc4354774159f6aa46e27c
James de Winton <james.dewinton@gmail.com>
2019-02-11 12:02:03 +0000

    auth file created

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import LoginForm from "../forms/LoginForm";
+import { login } from '../../actions/auth';
 
 class LoginPage extends React.Component {
-  submit = data => {
-    console.log(data);
-  };
+  submit = data => this.props.login(data).then(() => this.props.history.push("/"));
   render() {
     return (
       <div>
@@ -16,5 +17,10 @@ class LoginPage extends React.Component {
     );
   }
 }
-
-export default LoginPage;
+LoginPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired,
+  login: PropTypes.func.isRequired
+};
+export default connect(null, { login })(LoginPage);
--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
+
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
